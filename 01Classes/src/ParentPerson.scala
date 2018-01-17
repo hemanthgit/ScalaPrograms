2f5aa478a238943855269a04d43a2ceaea6b2b80
Hemanth <nagahemanthm@gmail.com>
2018-01-17 16:52:38 +0530

    Update ParentPerson.scala

--- 01Classes/src/ParentPerson.scala
+++ 01Classes/src/ParentPerson.scala
@@ -1,4 +1,22 @@
 class ParentPerson {
-  var name : String = "<No Name>";
-  var age : Int = 0;
+  println("Inside parent class Primary constructor")
+
+  var name: String = "<No Name>";
+  var age: Int = 0;
+
+  def this(name: String) {
+    this();
+    println("Inside Parent one-arg auxiliary constructor (name)")
+  }
+
+  def this(age: Int) {
+    this();
+    println("Inside Parent one-arg auxiliary constructor (age)")
+  }
+
+  def this(name: String, age: Int) {
+    this(name);
+    println("Inside Parent two-arg auxiliary constructor (name,age)")
+  }
+  println("STILL Inside parent class Primary constructor")
 }
